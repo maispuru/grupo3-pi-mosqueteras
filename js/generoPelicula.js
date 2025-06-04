ac573641d22c3a685452518b19eb982c5514f91f
rvillafane1 <rvillafane@udesa.edu.ar>
2025-06-04 16:47:44 -0300

    hola

--- js/generoPelicula.js
+++ js/generoPelicula.js
@@ -1,7 +1,6 @@
 window.addEventListener("load", function () {
     const API_KEY = "9731aaf98dbc7db52a32fb77a340e7c4"; 
     const BASE_URL = "https://api.themoviedb.org/3";
-    
     const API_GENRE_MOVIE = `${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=es-ES`;
 
     fetch(API_GENRE_MOVIE)
@@ -12,7 +11,8 @@ window.addEventListener("load", function () {
             const contenedor = document.querySelector(".maingenero");
             let genresHTML = '';
 
-            data.genres.forEach(function (genre) {
+            for (let i = 0; i < data.genres.length; i++) {
+                const genre = data.genres[i];
                 genresHTML += `
                     <section class="generopelicula">
                         <a class="link" href="./detallegenero.html?genre_id=${genre.id}&genre_name=${encodeURIComponent(genre.name)}">
@@ -20,7 +20,7 @@ window.addEventListener("load", function () {
                         </a>
                     </section>
                 `;
-            });
+            }
 
             contenedor.innerHTML = genresHTML;
         })
@@ -30,6 +30,8 @@ window.addEventListener("load", function () {
             contenedor.innerHTML = '<p>Error cargando géneros. Intenta más tarde.</p>';
         });
 });
+ 
+
 
 
 
---
+++ js/generoSerie.js
@@ -0,0 +1,37 @@
+window.addEventListener("load", function () {
+    const API_KEY = "9731aaf98dbc7db52a32fb77a340e7c4"; 
+    const BASE_URL = "https://api.themoviedb.org/3";
+    const API_GENRE_SERIE = `${BASE_URL}/genre/tv/list?api_key=${API_KEY}&language=es-ES`;
+
+
+    fetch(API_GENRE_SERIE)
+        .then(function (respuesta) {
+            return respuesta.json();
+        })
+        .then(function (data) {
+            const contenedor = document.querySelector(".maingenero");
+            let genresHTML = '';
+
+            for (let i = 0; i < data.genres.length; i++) {
+                const genre = data.genres[i];
+                genresHTML += `
+                    <section class="generoserie">
+                        <a class="link" href="./detallegenero.html?genre_id=${genre.id}&genre_name=${encodeURIComponent(genre.name)}">
+                            <h2 class="titulosgeneroserie">${genre.name}</h2>
+                        </a>
+                    </section>
+                `;
+            }
+
+            contenedor.innerHTML = genresHTML;
+        })
+        .catch(function (error) {
+            console.log("Error al cargar géneros:", error);
+            const contenedor = document.querySelector(".maingenero");
+            contenedor.innerHTML = '<p>Error cargando géneros. Intenta más tarde.</p>';
+        });
+});
+
+
+  
+  
