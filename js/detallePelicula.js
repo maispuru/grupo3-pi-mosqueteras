4927bd94b48a0200486e06204b129f3dd29045c5
maispuru <maispuru@udesa.edu.ar>
2025-06-05 18:24:27 -0300

    detalles js

--- js/detallePelicula.js
+++ js/detallePelicula.js
@@ -2,31 +2,36 @@ window.addEventListener("load", function () {
   const API_KEY = "9731aaf98dbc7db52a32fb77a340e7c4";
   const BASE_URL = "https://api.themoviedb.org/3";
 
-  const queryString = location.search;
-  const params = new URLSearchParams(queryString);
-  const idPelicula = params.get("id");
+  let queryString = location.search;
+  let queryStringObj  = new URLSearchParams(queryString);
+  const idPelicula = queryStringObj.get("id");
 
 let titulo = document.querySelector ("h1")
 let imagen = document.querySelector (".imagen")
 let clasificacion = document.querySelector (".Clasificacion-detalles")
-let estreno = document.querySelector (".estreno.detalles")
+let estreno = document.querySelector (".estreno-detalles")
 let genero = document.querySelector (".genero-detalles")
 let sinopis = document.querySelector (".Sinopsis-detalles")
 
-  const url = `${BASE_URL}/movie/${idPelicula}?api_key=${API_KEY}&language=es-EN`;
+ const url = `${BASE_URL}/movie/${idPelicula}?api_key=${API_KEY}&language=es-ES`;
 
-
-  fetch(url)
+fetch(url)
     .then(function (respuesta) {
       return respuesta.json();
     })
     .then(function (data) {
       titulo.innerText = data.title;
       clasificacion.innerHTML +=`${data.vote_average}`;
-      
-
+      estreno.innerHTML += `${data.release_date}`;
+      sinopis.innerHTML += `${data.overview}`;
+      let generos = "";
+for (let i = 0; i < data.genres.length; i++) {
+  let generoActual = data.genres[i];
+  generos += `<a class="link-detalles" href="./detalleGenero.html?id=${generoActual.id}&type=movie">${generoActual.name}</a> `;
+}
+     genero.innerHTML += generos;
     })
-    .catch(function (error) {
-      console.log("Error:", error);
-    });
-});
+.catch(function (error) {
+  console.log("Error:", error);
+})
+})
